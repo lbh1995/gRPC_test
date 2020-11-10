ac028f4fae334d6f0769d807f9563298c1ee17bd
lbh1995 <1044072893@qq.com>
2020-11-10 10:41:36 +0800

    the project of GRPC learning

---
+++ src/main/java/com/example/lbh/LbhApplication.java
@@ -0,0 +1,13 @@
+package com.example.lbh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LbhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lbh/LbhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lbh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
